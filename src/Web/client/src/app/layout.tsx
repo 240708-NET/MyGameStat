6a4e6146b6241bc6b70d541591427fc8f7783dbd
Bilal Mirza <bbmirza117@gmail.com>
2024-08-15 16:46:01 -0500

    changed file structure, footer, updated nav bar

--- src/Web/client/src/app/layout.tsx
+++ src/Web/client/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavLinks from './NavBar/NavLink'
+import NavLinks from './Components/NavBar/NavLink'
+import Footer from './Components/Footer/Footer'
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,9 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
       <body className={inter.className}>
         <NavLinks/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
--- src/Web/client/src/app/page.tsx
+++ src/Web/client/src/app/page.tsx
@@ -2,9 +2,12 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+
+
   return (
     <div className = {styles.Container}>
       <p> Home Sweet Home.</p>
     </div>
   );
+
 }
