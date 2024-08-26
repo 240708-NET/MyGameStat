58581a8fcbad91f57741abebadd2050c7ba4c9c4
Tamekia <tamekialnelson@gmail.com>
2024-08-26 10:44:50 -0400

    wishlist page external api and css

--- src/Web/client/src/app/Pages/Wishlist/page.tsx
+++ src/Web/client/src/app/Pages/Wishlist/page.tsx
@@ -1,20 +1,50 @@
-/*
-desc
-*/
-
 'use client';
 
-import React from 'react';
-import styles from './Wishlist.module.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import WishlistSlider from '../../Components/WishlistSlider/WishlistSlider';
+import styles from './Wishlist.module.css'; 
 
 const WishlistPage: React.FC = () => {
-    return (
-        <main>
-            <div className = {styles.Container}>
-                <p>Keep on wishin...</p>
-            </div>
-      </main>
-    )
+    const [wishlistItems, setWishlistItems] = useState<any[]>([]);
+    const [loading, setLoading] = useState(true); 
+
+    useEffect(() => {
+        const fetchWishlist = async () => {
+            const API_KEY = '020647103efa4c01ab3418ae762fd875'; 
+            try {
+                const response = await axios.get('https://api.rawg.io/api/games', {
+                    params: {
+                        key: API_KEY,
+                    },
+                });
+                setWishlistItems(response.data.results);
+                setLoading(false); 
+            } catch (error) {
+                console.error('Error fetching wishlist:', error);
+                setLoading(false); 
+            }
+        };
+
+        fetchWishlist(); 
+    }, []);
+
+    return (   
+            <main>
+                <div className={styles.Title}>
+                    <h1>Wishlist</h1>
+                </div>
+                <div className={styles.Container}>
+                    {loading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        <WishlistSlider wishlistItems={wishlistItems} /> 
+                    )}
+                </div>
+            </main>
+    );
 };
 
 export default WishlistPage;
+
+
